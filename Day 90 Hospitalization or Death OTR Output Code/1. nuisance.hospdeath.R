755d3ccb1ea30ade066eb0b29e6c9148bafdac50
saraskim <113139464+saraskim@users.noreply.github.com>
2024-11-25 14:07:00 -0600

    Add files via upload
    Initial commit of day 90 hospitalization or death OTR output code

---
+++ Day 90 Hospitalization or Death OTR Output Code/1. nuisance.hospdeath.R
@@ -0,0 +1,148 @@
+# -------------------------------------------------------------------------------------------
+# 
+# Code for ABCD Trial personalized treatment effects 
+# Nuisance model code for DAY 90 RE-HOSPITALIZATION or Death outcome
+#
+#
+# Sara Kim
+# 28 January 2024
+#
+# ------------------------------------------------------------------------------------------
+
+# ----- Make sure "0. wrappers.hospdeath.R" script has been run
+
+# ----- Load libraries
+library(SuperLearner)
+library(dplyr)
+library(drotr)
+library(ranger)
+library(earth)
+library(glmnet)
+library(stats)
+library(xgboost)
+
+
+# ----- Load dataset 
+# Deidentified dataset available upon reasonable request with data use agreement 
+abcd_data <- read.csv("abcd_otr_deidentified.csv")[,-1] 
+# Ensure categorical variables are factor format
+abcd_data$site <- as.factor(abcd_data$site)
+abcd_data$dy1_scrn_dehydr <- as.factor(abcd_data$dy1_scrn_dehydr)
+abcd_data$dy1_ant_sex <- as.factor(abcd_data$dy1_ant_sex)
+abcd_data$an_ses_quintile <- as.factor(abcd_data$an_ses_quintile)
+abcd_data$dy1_scrn_vomitall <- as.factor(abcd_data$dy1_scrn_vomitall)
+abcd_data$month_en <- as.factor(abcd_data$month_en)
+
+
+# ----- Run nuisance models over five seeds to get pseudo-outcomes
+
+# set seed
+set.seed(NULL)
+set.seed(150)
+
+# Run nuisance model 
+nuisance_output <- learn_nuisance(df = abcd_data,
+                                  Y_name = "an_hosp90death",
+                                  A_name = "an_grp_01",
+                                  W_list = W_list,
+                                  id_name = "pid", 
+                                  sl.library.outcome = sl.library.outcome,
+                                  sl.library.treatment = sl.library.treatment,
+                                  sl.library.missingness = sl.library.missingness,
+                                  outcome_type = "binomial",
+                                  k_folds = 10,
+                                  ps_trunc_level = 0.01)
+
+nuisance_models <- nuisance_output$nuisance_models
+
+# Save nuisance output
+saveRDS(nuisance_output, file="nuisance150_hospdeath_24apr2024.RData")
+
+# set seed
+set.seed(NULL)
+set.seed(300)
+
+# Run nuisance model 
+nuisance_output <- learn_nuisance(df = abcd_data,
+                                  Y_name = "an_hosp90death",
+                                  A_name = "an_grp_01",
+                                  W_list = W_list,
+                                  id_name = "pid", 
+                                  sl.library.outcome = sl.library.outcome,
+                                  sl.library.treatment = sl.library.treatment,
+                                  sl.library.missingness = sl.library.missingness,
+                                  outcome_type = "binomial",
+                                  k_folds = 10,
+                                  ps_trunc_level = 0.01)
+
+nuisance_models <- nuisance_output$nuisance_models
+
+# Save nuisance output
+saveRDS(nuisance_output, file="nuisance300_hospdeath_24apr2024.RData")
+
+# set seed
+set.seed(NULL)
+set.seed(450)
+
+# Run nuisance model 
+nuisance_output <- learn_nuisance(df = abcd_data,
+                                  Y_name = "an_hosp90death",
+                                  A_name = "an_grp_01",
+                                  W_list = W_list,
+                                  id_name = "pid", 
+                                  sl.library.outcome = sl.library.outcome,
+                                  sl.library.treatment = sl.library.treatment,
+                                  sl.library.missingness = sl.library.missingness,
+                                  outcome_type = "binomial",
+                                  k_folds = 10,
+                                  ps_trunc_level = 0.01)
+
+nuisance_models <- nuisance_output$nuisance_models
+
+# Save nuisance output
+saveRDS(nuisance_output, file="nuisance450_hospdeath_24apr2024.RData")
+
+# set seed
+set.seed(NULL)
+set.seed(600)
+
+# Run nuisance model 
+nuisance_output <- learn_nuisance(df = abcd_data,
+                                  Y_name = "an_hosp90death",
+                                  A_name = "an_grp_01",
+                                  W_list = W_list,
+                                  id_name = "pid", 
+                                  sl.library.outcome = sl.library.outcome,
+                                  sl.library.treatment = sl.library.treatment,
+                                  sl.library.missingness = sl.library.missingness,
+                                  outcome_type = "binomial",
+                                  k_folds = 10,
+                                  ps_trunc_level = 0.01)
+
+nuisance_models <- nuisance_output$nuisance_models
+
+# Save nuisance output
+saveRDS(nuisance_output, file="nuisance600_hospdeath_24apr2024.RData")
+
+# set seed
+set.seed(NULL)
+set.seed(750)
+
+# Run nuisance model 
+nuisance_output <- learn_nuisance(df = abcd_data,
+                                  Y_name = "an_hosp90death",
+                                  A_name = "an_grp_01",
+                                  W_list = W_list,
+                                  id_name = "pid", 
+                                  sl.library.outcome = sl.library.outcome,
+                                  sl.library.treatment = sl.library.treatment,
+                                  sl.library.missingness = sl.library.missingness,
+                                  outcome_type = "binomial",
+                                  k_folds = 10,
+                                  ps_trunc_level = 0.01)
+
+nuisance_models <- nuisance_output$nuisance_models
+
+# Save nuisance output
+saveRDS(nuisance_output, file="nuisance750_hospdeath_24apr2024.RData")
+
---
+++ Day 90 Hospitalization or Death OTR Output Code/5. clin.hospdeath.R
@@ -0,0 +1,230 @@
+# -------------------------------------------------------------------------------------------
+# 
+# Code for ABCD Trial personalized treatment effects 
+# Treatment effects for the Clinical Symptoms code for DAY 90 REHOSPITALIZATION OR DEATH outcome
+#
+#
+# Sara Kim
+# 28 January 2024
+#
+# ------------------------------------------------------------------------------------------
+
+# Please ensure "0. wrappers.hospdeath.R" script has been run and outputs from "1. nuisance.hospdeath.R" are in working directory
+
+# Load libraries
+library(SuperLearner)
+library(dplyr)
+library(drotr)
+library(ranger)
+library(earth)
+library(glmnet)
+library(stats)
+library(xgboost)
+
+
+# Load dataset 
+abcd_data <- read.csv("abcd_otr_deidentified.csv")[,-1] 
+abcd_data$site <- as.factor(abcd_data$site)
+abcd_data$dy1_scrn_dehydr <- as.factor(abcd_data$dy1_scrn_dehydr)
+abcd_data$dy1_ant_sex <- as.factor(abcd_data$dy1_ant_sex)
+abcd_data$an_ses_quintile <- as.factor(abcd_data$an_ses_quintile)
+abcd_data$dy1_scrn_vomitall <- as.factor(abcd_data$dy1_scrn_vomitall)
+abcd_data$month_en <- as.factor(abcd_data$month_en)
+
+
+# ----- Define CATE models to estimate child level expected benefit for the Clinical Symptoms rule
+
+
+# clinical symptoms
+SL.cate.clin <- function(Y, X, newX, family, ...){
+  sl.cate.clin_fit <- glm(Y ~ dy1_scrn_vomitall + dy1_scrn_lstools + dy1_scrn_sstools + 
+                            dy1_scrn_diardays + dy1_scrn_dehydr, 
+                          data = X,
+                          family = family)
+  # get predictions on newX
+  pred <- predict(
+    sl.cate.clin_fit, newdata = newX, type = 'response'
+  )
+  # format the output as named list
+  fit <- list(fitted_model.cate.clin = sl.cate.clin_fit)
+  out <- list(fit = fit, pred = pred)
+  # give the object a class
+  class(out$fit) <- "SL.cate.clin"
+  # return the output
+  return(out)
+}
+predict.SL.cate.clin <- function(object, newdata, ...){
+  pred <- predict(object$fitted_model.cate.clin, newdata = newdata, type = 'response')
+  return(pred)
+}
+
+# clinical symptoms with pairwise interaction
+SL.cate.clin2 <- function(Y, X, newX, family, ...){
+  sl.cate.clin2_fit <- glm(Y ~ (dy1_scrn_vomitall + dy1_scrn_lstools + dy1_scrn_sstools + 
+                                  dy1_scrn_diardays + dy1_scrn_dehydr)^2, 
+                           data = X,
+                           family = family)
+  # get predictions on newX
+  pred <- predict(
+    sl.cate.clin2_fit, newdata = newX, type = 'response'
+  )
+  # format the output as named list
+  fit <- list(fitted_model.cate.clin2 = sl.cate.clin2_fit)
+  out <- list(fit = fit, pred = pred)
+  # give the object a class
+  class(out$fit) <- "SL.cate.clin2"
+  # return the output
+  return(out)
+}
+predict.SL.cate.clin2 <- function(object, newdata, ...){
+  pred <- predict(object$fitted_model.cate.clin2, newdata = newdata, type = 'response')
+  return(pred)
+}
+
+
+# ------ Define CATE model superlearner library 
+sl.library.CATE <- c("SL.glm", "SL.glmnet", "SL.ranger", "SL.earth", "SL.xgboost", "SL.glmnet.twoway",
+                     "SL.cate.clin", "SL.cate.clin2")
+
+# ------ Define variables in the clinical symptoms rule 
+Z_lists <- c("dy1_scrn_vomitall", "dy1_scrn_lstools", "dy1_scrn_sstools", "dy1_scrn_diardays",
+             "dy1_scrn_dehydr")
+
+
+# ----- Run estimation for child level expected benefit over five seeds
+
+# seed 150
+nuisance_output <- readRDS("nuisance150_hospdeath_24apr2024.RData")
+nuisance_models <- nuisance_output$nuisance_models
+k_fold_assign_and_CATE <- nuisance_output$k_fold_assign_and_CATE
+validRows <- nuisance_output$validRows
+
+set.seed(NULL)
+set.seed(150)
+
+results_hosp_clin150 <- estimate_OTR(df = abcd_data,
+                                       Y_name = "an_hosp90death",
+                                       A_name = "an_grp_01",
+                                       W_list = W_list,
+                                       Z_list = Z_lists,
+                                       id_name = "pid",
+                                       sl.library.CATE = sl.library.CATE,
+                                       nuisance_models = nuisance_models,
+                                       validRows = validRows,
+                                       k_fold_assign_and_CATE = k_fold_assign_and_CATE,
+                                       threshold = -0.02,
+                                       k_folds = 10,
+                                       ps_trunc_level = 0.01,
+                                       outcome_type = "binomial")
+
+saveRDS(results_hosp_clin150$results, "results_hosp_clin150.RData")
+saveRDS(results_hosp_clin150$CATE_models, "catemodels_hosp_clin150.RData")
+
+# seed 300
+nuisance_output <- readRDS("nuisance300_hospdeath_24apr2024.RData")
+nuisance_models <- nuisance_output$nuisance_models
+k_fold_assign_and_CATE <- nuisance_output$k_fold_assign_and_CATE
+validRows <- nuisance_output$validRows
+
+set.seed(NULL)
+set.seed(300)
+
+results_hosp_clin300 <- estimate_OTR(df = abcd_data,
+                                     Y_name = "an_hosp90death",
+                                     A_name = "an_grp_01",
+                                     W_list = W_list,
+                                     Z_list = Z_lists,
+                                     id_name = "pid",
+                                     sl.library.CATE = sl.library.CATE,
+                                     nuisance_models = nuisance_models,
+                                     validRows = validRows,
+                                     k_fold_assign_and_CATE = k_fold_assign_and_CATE,
+                                     threshold = -0.02,
+                                     k_folds = 10,
+                                     ps_trunc_level = 0.01,
+                                     outcome_type = "binomial")
+
+saveRDS(results_hosp_clin300$results, "results_hosp_clin300.RData")
+saveRDS(results_hosp_clin300$CATE_models, "catemodels_hosp_clin300.RData")
+
+# seed 450
+nuisance_output <- readRDS("nuisance450_hospdeath_24apr2024.RData")
+nuisance_models <- nuisance_output$nuisance_models
+k_fold_assign_and_CATE <- nuisance_output$k_fold_assign_and_CATE
+validRows <- nuisance_output$validRows
+
+set.seed(NULL)
+set.seed(450)
+
+results_hosp_clin450 <- estimate_OTR(df = abcd_data,
+                                     Y_name = "an_hosp90death",
+                                     A_name = "an_grp_01",
+                                     W_list = W_list,
+                                     Z_list = Z_lists,
+                                     id_name = "pid",
+                                     sl.library.CATE = sl.library.CATE,
+                                     nuisance_models = nuisance_models,
+                                     validRows = validRows,
+                                     k_fold_assign_and_CATE = k_fold_assign_and_CATE,
+                                     threshold = -0.02,
+                                     k_folds = 10,
+                                     ps_trunc_level = 0.01,
+                                     outcome_type = "binomial")
+
+saveRDS(results_hosp_clin450$results, "results_hosp_clin450.RData")
+saveRDS(results_hosp_clin450$CATE_models, "catemodels_hosp_clin450.RData")
+
+# seed 600
+nuisance_output <- readRDS("nuisance600_hospdeath_24apr2024.RData")
+nuisance_models <- nuisance_output$nuisance_models
+k_fold_assign_and_CATE <- nuisance_output$k_fold_assign_and_CATE
+validRows <- nuisance_output$validRows
+
+set.seed(NULL)
+set.seed(600)
+
+results_hosp_clin600 <- estimate_OTR(df = abcd_data,
+                                     Y_name = "an_hosp90death",
+                                     A_name = "an_grp_01",
+                                     W_list = W_list,
+                                     Z_list = Z_lists,
+                                     id_name = "pid",
+                                     sl.library.CATE = sl.library.CATE,
+                                     nuisance_models = nuisance_models,
+                                     validRows = validRows,
+                                     k_fold_assign_and_CATE = k_fold_assign_and_CATE,
+                                     threshold = -0.02,
+                                     k_folds = 10,
+                                     ps_trunc_level = 0.01,
+                                     outcome_type = "binomial")
+
+saveRDS(results_hosp_clin600$results, "results_hosp_clin600.RData")
+saveRDS(results_hosp_clin600$CATE_models, "catemodels_hosp_clin600.RData")
+
+# seed 750
+nuisance_output <- readRDS("nuisance750_hospdeath_24apr2024.RData")
+nuisance_models <- nuisance_output$nuisance_models
+k_fold_assign_and_CATE <- nuisance_output$k_fold_assign_and_CATE
+validRows <- nuisance_output$validRows
+
+set.seed(NULL)
+set.seed(750)
+
+results_hosp_clin750 <- estimate_OTR(df = abcd_data,
+                                     Y_name = "an_hosp90death",
+                                     A_name = "an_grp_01",
+                                     W_list = W_list,
+                                     Z_list = Z_lists,
+                                     id_name = "pid",
+                                     sl.library.CATE = sl.library.CATE,
+                                     nuisance_models = nuisance_models,
+                                     validRows = validRows,
+                                     k_fold_assign_and_CATE = k_fold_assign_and_CATE,
+                                     threshold = -0.02,
+                                     k_folds = 10,
+                                     ps_trunc_level = 0.01,
+                                     outcome_type = "binomial")
+
+saveRDS(results_hosp_clin750$results, "results_hosp_clin750.RData")
+saveRDS(results_hosp_clin750$CATE_models, "catemodels_hosp_clin750.RData")
+
